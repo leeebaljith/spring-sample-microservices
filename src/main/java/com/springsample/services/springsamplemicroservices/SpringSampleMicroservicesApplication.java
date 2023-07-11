ce53a73d6e369003344451165deff23836b4e93a
leeebaljith <balajisaravanan97@gmail.com>
2023-07-11 13:05:04 +0530

    1st base commit for spring microservices

---
+++ src/main/java/com/springsample/services/springsamplemicroservices/SpringSampleMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.springsample.services.springsamplemicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampleMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSampleMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsample/services/springsamplemicroservices/SpringSampleMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsample.services.springsamplemicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
